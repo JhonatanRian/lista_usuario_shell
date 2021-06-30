170ac8f2ab0cdea5c2cebe1eb4a5019c557ac2e6
Jhonatan Rian <64628254+JhonatanRian@users.noreply.github.com>
2021-06-30 02:33:39 -0300

    Primeiro script em shel

---
+++ lista_usuario.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#!/usr/bin/env bash
+
+#
+
+# lista_usuario.sh - Extrai usuarios do /etc/passwd 
+
+#
+
+# Site:       https://github.com/JhonatanRian
+
+# Autor:      Jhonatan Rian
+
+# ------------------------------------------------------------------------ #
+
+#  Extrai nomes do usuario com possibilidade de colocar em maiusculo
+#  Ou em ordem alfabetica
+
+#  Exemplos:
+
+#      $ lista_usuario.sh -s -m
+
+#      Neste exemplo o script será executado no modo debug nível 1
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
+MENSAGEM="
+    $0 - [OPÇÕES]
+
+    -h - menu de ajuda
+    -v - versão
+    -s - ordenar a saida
+"
+VERSAO="1.0"
+
+# ------------------------------------------------------------------------ #
+
+
+# ------------------------------- TESTES ----------------------------------------- #
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+if [ "$1" = "-h" ]; then
+    echo "$MENSAGEM" && exit 0
+elif [ "$1" = "-v" ]; then
+    echo "$VERSAO" && exit
+elif [ "$1" = "-s" ]; then
+    echo "$USUARIOS" | sort && exit
+else
+    echo ""
+fi
+
+echo "$USUARIOS"
+# ------------------------------------------------------------------------ #
